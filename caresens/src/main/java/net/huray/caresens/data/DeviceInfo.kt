24ae609ab21b0fa20fd45f6f576e2e833deada21
Trung, Ngo Van <trung.ngo1@ntq-solution.com.vn>
2023-11-30 17:49:54 +0700

    fix device info 3

--- caresens/src/main/java/net/huray/caresens/data/DeviceInfo.kt
+++ caresens/src/main/java/net/huray/caresens/data/DeviceInfo.kt
@@ -1,8 +1,8 @@
 package net.huray.caresens.data
 
 public class DeviceInfo {
-    var name: String? = null
-    var serialNumber: String? = null
+    public var name: String? = null
+    public var serialNumber: String? = null
     var version: String? = null
     var totalCount: Int? = null
 }
